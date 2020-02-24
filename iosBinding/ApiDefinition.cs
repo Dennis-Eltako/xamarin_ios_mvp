3915b70c3f1edadb695037e5bda0e73f0c117203
Dennis Kreuzer <dk098@hdm-stuttgart.de>
2020-02-24 02:04:10 +0100

    initial commit

---
+++ iosBinding/ApiDefinition.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using CoreGraphics;
+using Foundation;
+using UIKit;
+
+namespace Xamarin.Bindings.iosUI
+{
+	// @interface LightControl : UIView
+	[BaseType(typeof(UIView), Name = "_TtC5iosUI12LightControl")]
+	interface LightControl
+	{
+		// -(instancetype _Nonnull)initWithFrame:(CGRect)frame __attribute__((objc_designated_initializer));
+		[Export("initWithFrame:")]
+		[DesignatedInitializer]
+		IntPtr Constructor(CGRect frame);
+
+		// -(void)setImageWithImage:(UIImage * _Nonnull)image;
+		[Export("setImageWithImage:")]
+		void SetImageWithImage(UIImage image);
+
+		// -(void)setLabelTextWithText:(NSString * _Nonnull)text;
+		[Export("setLabelTextWithText:")]
+		void SetLabelTextWithText(string text);
+
+		// -(void)setSwitchValueWithValue:(BOOL)value;
+		[Export("setSwitchValueWithValue:")]
+		void SetSwitchValueWithValue(bool value);
+	}
+}
---
+++ xamarin_ios_mvp/ViewController.cs
@@ -0,0 +1,52 @@
+﻿using Foundation;
+using System;
+using UIKit;
+using Xamarin.Bindings.iosUI;
+
+
+namespace xamarin_ios_mvp
+{
+    public partial class ViewController : UIViewController
+    {
+
+        LightControl lightControl;
+        UIImage image;
+        bool toggle = true;
+
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+            image = UIImage.FromBundle(getImage());
+            lightControl = new LightControl(View.Frame);
+            lightControl.SetLabelTextWithText("Living Room");
+            lightControl.SetImageWithImage(image);
+            lightControl.SetSwitchValueWithValue(toggle);
+            View.AddSubview(lightControl);
+
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        String getImage()
+        {
+            if (toggle)
+            {
+                return "lightOn.png";
+            }
+            else
+            {
+                return "lightOff.png";
+            }
+        }
+
+    }
+}
